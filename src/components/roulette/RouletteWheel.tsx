890b6914d36f690407f79197f79aaa9eab3712c5
DaeYoung0726 <pdy000726@naver.com>
2026-02-09 22:54:16 +0900

    feat: 로고 추가

--- src/components/roulette/RouletteWheel.tsx
+++ src/components/roulette/RouletteWheel.tsx
@@ -1,4 +1,5 @@
 import { useMemo } from 'react'
+const logo = '/favicon.svg'
 
 type RouletteWheelProps = {
   isSpinning: boolean
@@ -11,24 +12,27 @@ function RouletteWheel({ isSpinning, onSpinEnd }: RouletteWheelProps) {
   return (
     <div className="relative flex items-center justify-center">
       <div className="absolute top-[-6px] h-0 w-0 border-x-[12px] border-b-[22px] border-x-transparent border-b-black" />
-      <div
-        className={`relative h-72 w-72 rounded-full border-4 border-black ${
-          isSpinning ? 'animate-roulette-spin' : ''
-        }`}
-        onAnimationEnd={onSpinEnd}
-      >
-        <div className="absolute inset-0 rounded-full border-2 border-black" />
-        <div className="absolute inset-2 rounded-full border border-black" />
-        {segments.map((segment) => (
-          <div
-            key={segment}
-            className="absolute left-1/2 top-1/2 h-1 w-[130px] -translate-y-1/2 origin-left bg-black"
-            style={{ transform: `rotate(${segment * 30}deg)` }}
-          />
-        ))}
-        <div className="absolute inset-8 rounded-full border-2 border-black bg-white" />
-        <div className="absolute inset-[102px] rounded-full border-4 border-black bg-[#D6FF00]" />
-        <div className="absolute inset-[118px] rounded-full border-2 border-black bg-white" />
+      <div className="relative h-72 w-72">
+        <div
+          className={`absolute inset-0 rounded-full border-4 border-black ${
+            isSpinning ? 'animate-roulette-spin' : ''
+          }`}
+          onAnimationEnd={onSpinEnd}
+        >
+          <div className="absolute inset-0 rounded-full border-2 border-black" />
+          <div className="absolute inset-2 rounded-full border border-black" />
+          {segments.map((segment) => (
+            <div
+              key={segment}
+              className="absolute left-1/2 top-1/2 h-1 w-[130px] -translate-y-1/2 origin-left bg-black"
+              style={{ transform: `rotate(${segment * 30}deg)` }}
+            />
+          ))}
+          <div className="absolute inset-8 rounded-full border-2 border-black bg-white" />
+        </div>
+        <div className="absolute inset-[102px] overflow-hidden rounded-full border-4 border-black bg-white">
+          <img src={logo} alt="Roulette-Up" className="h-full w-full object-cover" />
+        </div>
       </div>
       <div className="absolute bottom-[-14px] h-3 w-24 rounded-full border border-black bg-white" />
     </div>
